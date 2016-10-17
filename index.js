8cb2c1878c8cbbc6a53bf96b45921b05f044f105
Tris Setiawan <tris.setiawan@yahoo.com>
2016-10-17 11:39:59 +0000

    fix namespace

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = {
-    auth: {
-        AccountManager: require("./src/managers/auth/account-manager"),
-        RoleManager: require("./src/managers/auth/role-manager")
-    },
-    core: {
-        SpotManager: require('./src/managers/core/spot-manager'),
-        SpotTypeManager: require('./src/managers/core/spot-type-manager')
+    managers: {
+        auth: {
+            AccountManager: require("./src/managers/auth/account-manager"),
+            RoleManager: require("./src/managers/auth/role-manager")
+        },
+        core: {
+            SpotManager: require('./src/managers/core/spot-manager'),
+            SpotTypeManager: require('./src/managers/core/spot-type-manager')
+        }
     }
 }
