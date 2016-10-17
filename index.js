988fcebd4f74fd36321d168e93d5d27a0d5b3937
Tris Setiawan <tris.setiawan@yahoo.com>
2016-10-17 11:21:19 +0000

    add auth & restructuring

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 module.exports = {
-    SpotManager: require('./src/managers/spot-manager'),
-    SpotTypeManager: require('./src/managers/spot-type-manager')
+    auth: {
+        AccountManager: require("./src/managers/auth/account-manager"),
+        RoleManager: require("./src/managers/auth/role-manager")
+    },
+    core: {
+        SpotManager: require('./src/managers/core/spot-manager'),
+        SpotTypeManager: require('./src/managers/core/spot-type-manager')
+    }
 }
