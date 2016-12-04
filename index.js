88ba7dc58d1d113993f93b919db7893ca3085c17
Tris Setiawan <tris.setiawan@yahoo.com>
2016-12-04 16:58:21 +0000

    export test/data/* to index

--- index.js
+++ index.js
@@ -8,5 +8,17 @@ module.exports = {
             SpotManager: require('./src/managers/core/spot-manager'),
             SpotTypeManager: require('./src/managers/core/spot-type-manager')
         }
+    },
+    test: {
+        data: {
+            auth: {
+                account: require("./test/data/auth/account-data-util"),
+                role: require("./test/data/auth/role-data-util")
+            },
+            core: {
+                spot: require("./test/data/core/spot-data-util"),
+                spotType: require("./test/data/core/spot-type-data-util")
+            }
+        }
     }
-}
+};
